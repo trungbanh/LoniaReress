cb620aafa360b55f6d3133189e21d41d30206cdc
trungbanh <trungbanh97@gmail.com>
2018-04-12 20:00:29 +0700

    sfdsf

---
+++ bt4.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+def lr2_t2(X,Y,t0,t1) :
+    
+    return h0,h1
+
+if __name__=="__main__" :
+    X = np.array([1,2,4])
+    Y = np.array([2,3,6])
+    e = 0.2
+    t0 =0 
+    t1 =1
+    
+
+    x = np.array([0,3,5])
+    for j in range(len(x)) :
+        y = h0+h1*x[j]
+        print("y ",y)
